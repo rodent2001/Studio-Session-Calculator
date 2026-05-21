dbdae586c9f55a8598078dbeb39ed5535e6561da
rodent2001 <rodent2001@gmail.com>
2026-05-21 12:54:58 +0300

    Update app to version 0.3.0

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -5,25 +5,13 @@ namespace StudioSessionOrderForm
 {
     public partial class MainWindow : Window
     {
-        // ViewModel
         private readonly MainWindowViewModel _viewModel;
 
-        // Class constructor
         public MainWindow()
         {
             InitializeComponent();
             _viewModel = new MainWindowViewModel();
             DataContext = _viewModel;
-        }
-
-        // Buttons handlers
-        private async void SaveOrderButton_Click(object sender, RoutedEventArgs e)
-        {
-            await _viewModel.SaveOrderAsync();
-        }
-        private void ResetDataButton_Click(object sender, RoutedEventArgs e)
-        {
-            _viewModel.ResetOrderData();
         }
     }
 }
