ae65bd6e5d8a61c02fa408f254f45748b9e673e1
rodent2001 <rodent2001@gmail.com>
2026-05-22 15:21:10 +0300

    Adding Commands directory with files

---
+++ Commands/AsyncRelayCommand.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows.Input;
+
+namespace StudioSessionOrderForm.Commands;
+
+public class AsyncRelayCommand : ICommand
+{
+
+    // Event and its raising method
+
+    public event EventHandler? CanExecuteChanged;
+
+    public void RaiseCanExecuteChanged()
+    {
+        CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+    // Class fields
+
+    private readonly Func<Task> _executeAsync;
+    private readonly Func<bool>? _canExecute;
+    private bool _isExecuting;
+
+    // Class constructor
+
+    public AsyncRelayCommand(Func<Task> executeAsync, Func<bool>? canExecute = null)
+    {
+        _executeAsync = executeAsync;
+        _canExecute = canExecute;
+    }
+
+    // Class methods
+
+    public bool CanExecute(object? parameter)
+    {
+        if (_isExecuting) return false;
+        else return _canExecute?.Invoke() ?? true;
+    }
+
+    public async void Execute(object? parameter)
+    {
+        if (!CanExecute(parameter)) return;
+
+        try
+        {
+            _isExecuting = true;
+            RaiseCanExecuteChanged();
+            await _executeAsync();
+        }
+        finally
+        {
+            _isExecuting = false;
+            RaiseCanExecuteChanged();
+        }
+    }
+}
---
+++ Commands/RelayCommand.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows.Input;
+
+namespace StudioSessionOrderForm.Commands;
+
+public class RelayCommand : ICommand
+{
+    // Event and its rising method
+
+    public event EventHandler? CanExecuteChanged;
+
+    public void RaiseCanExecuteChanged()
+    {
+        CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+    // Class fields
+
+    private readonly Action _execute;
+    private readonly Func<bool>? _canExecute;
+
+    // Class constructor
+    public RelayCommand(Action execute, Func<bool>? canExecute = null)
+    {
+        _execute = execute;
+        _canExecute = canExecute;
+    }
+
+    // Class methods
+
+    public void Execute(object? parameter)
+    {
+        _execute();
+    }
+
+    public bool CanExecute(object? parameter)
+    {
+        return _canExecute?.Invoke() ?? true;
+    }
+}
